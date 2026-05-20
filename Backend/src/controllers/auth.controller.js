92aa78132ed0c9c2de6b54c9ce8d380a3aa379e7
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-05-20 11:44:16 +0530

    chore: update node_modules dependencies and supporting files

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,15 +2,29 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
-async function sendTokenResponse(user,res){
-    const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {
+async function sendTokenResponse(user,res,message){
+    const token = jwt.sign({id:user._id}, config.JWT_SECRET, {
         expiresIn: "7d"
     });
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message,
+        success:true,
+        user:{
+            id:user._id,
+            email:user.email,
+            contact:user.contact,
+            fullname:user.fullname,
+            role:user.role
+        }
+    })
 }
 
 
 export const register = async (req,res) =>{
-    const {email, password, contact, fullname} = req.body;
+    const {email, password, contact, fullname, isSeller} = req.body;
 
     try{
         // Check if user already exists
@@ -30,8 +44,11 @@ export const register = async (req,res) =>{
             email,
             password,
             contact,
-            fullname
+            fullname,
+            role: isSeller ? "seller" : "buyer"
         });
+
+        await sendTokenResponse(user,res,"User registered successfully")
 
     }catch(error){
         console.log(error)
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import { validateRegisterUser } from "../validators/auth.validator.js";
+import { register } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post("/register", validateRegisterUser, (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "User registration successful",
-    data: req.body,
-  });
-});
+router.post("/register", validateRegisterUser, register);
+
 
 
 export default router;
